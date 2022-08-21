730b7062ef06e3a9eec0942361b58d98454a1417
wonkyDD <dev.csh.01@gmail.com>
2022-08-22 03:29:18 +0900

    :wrench: Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,106 +1,119 @@
-// https://velog.io/@xortm854/Typescript-React-Webpack-%ED%99%98%EA%B2%BD%EC%84%A4%EC%A0%95-4%ED%8E%B8-Webpack-%EC%84%A4%EC%A0%95%EC%9D%84-%ED%95%B4%EB%B3%B4%EC%9E%90
-// https://taenami.tistory.com/125
+/**
+ * @TODO
+ * package.json에 type을 module로 주면 webpack에러가 난다.
+ * 문제는 난 보통 repl로 테스트할때 루트경로에서 하는데..
+ * 이런식으로 에러가 나면
+ */
+
 const path = require('path');
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require('webpack');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const srcDir = path.join(__dirname, 'src');
 
 module.exports = (env, argv) => {
-    const prod = argv.mode === "production";
-    
-    return {
-        mode: prod ? "production" : "development",
-        // devtool: 'inline-source-map',(dev)
-        // devtool: prod ? "hidden-source-map" : "eval",
-        entry: {
-            popup: path.join(srcDir, 'popup.tsx'),
-            options: path.join(srcDir, 'options.tsx'),
-            background: path.join(srcDir, 'background.ts'),
-            content_script: path.join(srcDir, 'content_script.tsx'),
+  const prod = argv.mode === 'production';
+
+  return {
+    mode: prod ? 'production' : 'development',
+    // devtool: 'inline-source-map',(dev)
+    // devtool: prod ? "hidden-source-map" : "eval",
+    entry: {
+      popup: path.join(srcDir, 'popup.tsx'),
+      options: path.join(srcDir, 'options.tsx'),
+      background: path.join(srcDir, 'background.ts'),
+      content_script: path.join(srcDir, 'content_script.tsx'),
+      devtools: path.join(srcDir, 'devtools.ts'),
+    },
+    output: {
+      path: path.join(__dirname, './dist/'),
+      // filename: "bundle.min.js",
+      filename: '[name].js',
+    },
+    optimization: {
+      splitChunks: {
+        name: 'vendor',
+        chunks(chunk) {
+          return chunk.name !== 'background';
         },
-        output: {
-            path: path.join(__dirname, './dist/'),
-            // filename: "bundle.min.js",
-            filename: "[name].js",
+      },
+    },
+    // devServer: {
+    //     host: 'localhost',
+    //     port: 8080,
+    //     open: true,
+    //     hot: true,
+
+    //     // local에서 docker로 테스트서버 사용시에 3000포트로 was를 올려놓자
+    //     historyApiFallback: true,
+    //     proxy: {
+    //       '/api/': {
+    //         target: 'http://localhost:3000/api',
+    //         changeOrigin: true,
+    //         pathRewrite: {
+    //           '^/api': '',
+    //         },
+    //       },
+    //     },
+    // },
+    // node: {
+    //   global: false,
+    //   __filename: false,
+    //   __dirname: false,
+    //   fs: 'empty',
+    // },
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      fallback: {},
+    },
+    module: {
+      rules: [
+        // ts-loader의 옵션은 성능 향상을 위해서? babel-loader만 있어도 ok?
+        {
+          test: /\.(ts|tsx)$/,
+          use: [
+            'babel-loader',
+            {
+              loader: 'ts-loader',
+              options: {
+                transpileOnly: true,
+              },
+            },
+          ],
+          exclude: /node_modules/,
         },
-        // optimization: {
-        //     splitChunks: {
-        //       name: 'vendor',
-        //       chunks(chunk) {
-        //         return chunk.name !== 'background';
+        // {
+        //     test: /\.(png|jpe?g|gif|woff|woff2|ttf|svg|ico)$/i,
+        //     use: [
+        //       {
+        //         loader: 'file-loader',
         //       },
-        //     },
+        //     ],
+        //   },
+        // {
+        //     test: /\.css$/i,
+        //     use: ['style-loader', 'css-loader'],
         // },
-        // devServer: {
-        //     host: 'localhost',
-        //     port: 8080,
-        //     open: true,
-        //     hot: true,
-            
-        //     // local에서 docker로 테스트서버 사용시에 3000포트로 was를 올려놓자
-        //     historyApiFallback: true,
-        //     proxy: {
-        //       '/api/': {
-        //         target: 'http://localhost:3000/api',
-        //         changeOrigin: true,
-        //         pathRewrite: {
-        //           '^/api': '',
-        //         },
-        //       },
-        //     },
-        // },
-        resolve: {
-            extensions: [".js", ".jsx", ".ts", ".tsx"],
-        },
-        module: {
-            rules: [
-                // ts-loader의 옵션은 성능 향상을 위해서? babel-loader만 있어도 ok?
-                {
-                  test: /\.(ts|tsx)$/,
-                  use: [
-                    'babel-loader',
-                    {
-                      loader: 'ts-loader',
-                      options: {
-                        transpileOnly: true,
-                      },
-                    },
-                  ],
-                  exclude: /node_modules/,
-                },
-                // {
-                //     test: /\.(png|jpe?g|gif|woff|woff2|ttf|svg|ico)$/i,
-                //     use: [
-                //       {
-                //         loader: 'file-loader',
-                //       },
-                //     ],
-                //   },
-                // {
-                //     test: /\.css$/i,
-                //     use: ['style-loader', 'css-loader'],
-                // },
-            ],
-        },
-        plugins: [
-            new webpack.ProvidePlugin({
-                React: "react",
-            }),
-            // new HtmlWebpackPlugin({
-            //     template: './public/*.html',
-            //     minify: process.env.NODE_ENV === 'production' ? {
-            //         collapseWhitespace: true, 
-            //         removeComments: true, 
-            //     } : false,
-            // }),
-            new CleanWebpackPlugin(),
-            new CopyPlugin({
-                patterns: [{ from: '.', to: '.', context: 'public' }],
-                options: {},
-            }),
-        ],
-    }
+      ],
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        React: 'react',
+      }),
+      // new HtmlWebpackPlugin({
+      //     template: './public/*.html',
+      //     minify: process.env.NODE_ENV === 'production' ? {
+      //         collapseWhitespace: true,
+      //         removeComments: true,
+      //     } : false,
+      // }),
+      new CleanWebpackPlugin(),
+      new CopyPlugin({
+        patterns: [{ from: '.', to: '.', context: 'public' }],
+        options: {},
+      }),
+    ],
+  };
 };
