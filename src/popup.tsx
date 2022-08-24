93486513d52de535605cebbfde137033946cafb7
wonkyDD <dev.csh.01@gmail.com>
2022-08-24 22:42:46 +0900

    :art: Feat: add page component into popup using regex

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,26 +1,67 @@
 import React, { useState, useEffect } from 'react';
 import * as ReactDOMClient from 'react-dom/client';
+import styled from 'styled-components';
+import { Header, NavigationTab } from './components/common';
+import { Home, Twitter, Dcinside, Xvid, Kiss } from './pages';
+import { regexTwitterRanking, regexDcinside, regexKiss, regexXvid, regexYoutube, regexTwitch } from './regex';
 
-function Popup() {
-  const [response, setResponse] = useState<string>();
-  const regexTwitterRanking = /^(https?:\/\/)?(video.twimg.com)\//;
+const PopupBlock = styled.div``;
+
+interface Prop {
+  siteName: string | undefined;
+}
+
+const Page = (props: Prop) => {
+  const { siteName } = props;
+  switch (siteName) {
+    case 'Twitter':
+      return <Twitter />;
+    case 'Dcinside':
+      return <Dcinside />;
+    case 'Xvid':
+      return <Xvid />;
+    case 'Kiss':
+      return <Kiss />;
+    default:
+      return <Home />;
+  }
+};
+
+const Popup = () => {
+  const [siteName, setSiteName] = useState<string>();
+
+  /**
+   * @TODO
+   * youtube, twitch 처리
+   */
+  const checkSite = (url: string) => {
+    if (regexTwitterRanking.test(url)) setSiteName('Twitter');
+    else if (regexKiss.test(url)) setSiteName('Kiss');
+    else if (regexXvid.test(url)) setSiteName('Xvid');
+    else if (regexDcinside.test(url)) setSiteName('Dcinside');
+    else if (regexYoutube.test(url)) setSiteName('Youtube');
+    else if (regexTwitch.test(url)) setSiteName('Twitch');
+  };
 
   useEffect(() => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const { url } = tabs[0];
-      if (regexTwitterRanking.test(url || '')) {
-        setResponse(`Yes, It's twitter ranking vid link ${url}`);
-        chrome.runtime.sendMessage(url, (res: any) => {
-          console.log(res); // 아무런 효과 없는 거임
-        });
-      } else {
-        setResponse(`No, you are not on a twitter ranking site ${url}`);
-      }
+      const { url, id } = tabs[0];
+
+      checkSite(url || '');
+      chrome.tabs.sendMessage(id!, 'hello', (res: any) => {
+        console.log(res);
+      });
     });
   });
 
-  return <div>{response}</div>;
-}
+  return (
+    <PopupBlock>
+      <Header />
+      <NavigationTab />
+      <Page siteName={siteName} />
+    </PopupBlock>
+  );
+};
 
 const container = document.getElementById('root')!;
 const root = ReactDOMClient.createRoot(container);
---
+++ src/regex.ts
@@ -0,0 +1,8 @@
+const regexTwitterRanking = /^(https?:\/\/)?(video.twimg.com)\//;
+const regexDcinside = /^(https?:\/\/)?(gall.dcinside.com)(\/mgallery|\/mini)?\/board\/lists(\/)?\?id=/;
+const regexKiss = /^(https?:\/\/)?(kissjav.li)\/[\d]+\//;
+const regexXvid = /^(https?:\/\/)?(www.xvideos.com)\/(video[\d]+)\//;
+const regexYoutube = /^(https?:\/\/)?(www.youtube.com)\/watch\?v=/;
+const regexTwitch = /^(https?:\/\/)?(www.twitch.tv)\/([\w]+)/;
+
+export { regexTwitterRanking, regexDcinside, regexKiss, regexXvid, regexYoutube, regexTwitch };
